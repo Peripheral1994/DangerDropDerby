00fd1c073c474ca96fca4de2e5af4b4f17d28f31
Peter Richmond <dannydork10@gmail.com>
2015-12-02 12:23:59 -0800

    Added non-combatant team, added Grav Gun as dummy weapon to demonstrate functionality.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -2,3 +2,15 @@ AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 
 include( "shared.lua" )
+
+function GM:PlayerInitialSpawn( ply )
+    ply:SetTeam( 1 ) --Add the player to Non-Combatants
+end
+
+function GM:PlayerLoadout( ply )
+    
+    if ply:Team() == 1 then
+        ply:Give( "weapon_physcannon" )
+    end
+ 
+end
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -3,6 +3,8 @@ GM.Author = "Peter Richmond"
 GM.Email = "peter.h.richmond@gmail.com"
 GM.Website = "N/A"
 
+team.SetUp(1, "Non-Combatants", Color(255, 255, 0, 255))
+
 function GM:Initialize()
-  -- Do stuff
+  -- Do shit.
 end
